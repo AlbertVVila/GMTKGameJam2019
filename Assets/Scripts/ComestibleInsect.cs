0d20e4cec8de0ee021ddbd6ae5fc84c7812cdac3
Alfonso <alfonsomestres@gmail.com>
2019-08-04 11:01:00 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/ComestibleInsect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComestibleInsect : MonoBehaviour
+{
+    public bool triggered = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "tongue")
+        {
+            if (!triggered)
+            {
+                // only one behavior
+                Debug.Log("Triggered: " + this.name);
+                triggered = true;
+            } 
+
+        }
+
+        if (collision.name == "Player")
+        {
+            if (!triggered)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+    }
+
+}
